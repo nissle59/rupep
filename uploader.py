4a676e1b73f96bca4cff7ebc5533aefcc83d215f
nissle59 <nixncom@gmail.com>
2023-03-16 13:20:23 +0300

    Upd

--- uploader.py
+++ uploader.py
@@ -593,7 +593,7 @@ def upload_persons_full(limit=200):
 
 if __name__ == '__main__':
     logging.info(dtnow)
-    upload_companies()
+    #upload_companies()
     upload_persons_base()
     generate_persons_compare_file()
     load_kyc_companies()
